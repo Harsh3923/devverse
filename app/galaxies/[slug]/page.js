e542a39ca8ed632624438189a1a5ec8259b0ced5
Harsh <h16patel@torontomu.ca>
2026-03-28 11:43:19 -0400

    Force dynamic rendering on galaxy detail page

--- app/galaxies/[slug]/page.js
+++ app/galaxies/[slug]/page.js
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
+
+export const dynamic = "force-dynamic";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 import { supabase } from "@/lib/supabase";
